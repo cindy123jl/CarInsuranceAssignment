999e9f3b2575af5df6377765476cadcafb8295d9
cindy123jl <cindy123jl@gmail.com>
2021-05-27 18:57:58 -0700

    initial-commit

--- CarInsuranceAssignment/Controllers/AdminController.cs
+++ CarInsuranceAssignment/Controllers/AdminController.cs
@@ -17,119 +17,31 @@ namespace CarInsuranceAssignment.Controllers
         // GET: Admin
         public ActionResult Index()
         {
-            using (InsuranceEntities db = new InsuranceEntities())
-            {
-                var quotesInfo = db.Insurees;
-                var quotes = new List<Insuree>();
-                foreach (var quote in quotesInfo)
-                {
-                    var quoteInfo = new Insuree();
-                    quoteInfo.Id = quote.Id;
-                    quoteInfo.FirstName = quote.FirstName;
-                    quoteInfo.LastName = quote.LastName;
-                    quoteInfo.EmailAddress = quote.EmailAddress;
-                    quoteInfo.Quote = quote.Quote;
-                    quotes.Add(quoteInfo);
-                }
-                return View(quotes);
-            }
-            
+            return View(db.Insurees.ToList());
         }
 
         // GET: Admin/Details/5
-        public ActionResult Details(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Insuree insuree = db.Insurees.Find(id);
-            if (insuree == null)
-            {
-                return HttpNotFound();
-            }
-            return View(insuree);
-        }
-
+    
         // GET: Admin/Create
-        public ActionResult Create()
-        {
-            return View();
-        }
+       
 
         // POST: Admin/Create
         // To protect from overposting attacks, enable the specific properties you want to bind to, for 
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "Id,FirstName,LastName,EmailAddress,DateOfBirth,CarYear,CarMake,CarModel,DUI,SpeedingTickets,CoverageType,Quote")] Insuree insuree)
-        {
-            if (ModelState.IsValid)
-            {
-                db.Insurees.Add(insuree);
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
-
-            return View(insuree);
-        }
-
+       
         // GET: Admin/Edit/5
-        public ActionResult Edit(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Insuree insuree = db.Insurees.Find(id);
-            if (insuree == null)
-            {
-                return HttpNotFound();
-            }
-            return View(insuree);
-        }
+       
 
         // POST: Admin/Edit/5
         // To protect from overposting attacks, enable the specific properties you want to bind to, for 
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "Id,FirstName,LastName,EmailAddress,DateOfBirth,CarYear,CarMake,CarModel,DUI,SpeedingTickets,CoverageType,Quote")] Insuree insuree)
-        {
-            if (ModelState.IsValid)
-            {
-                db.Entry(insuree).State = EntityState.Modified;
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
-            return View(insuree);
-        }
+       
 
         // GET: Admin/Delete/5
-        public ActionResult Delete(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Insuree insuree = db.Insurees.Find(id);
-            if (insuree == null)
-            {
-                return HttpNotFound();
-            }
-            return View(insuree);
-        }
+       
 
         // POST: Admin/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public ActionResult DeleteConfirmed(int id)
-        {
-            Insuree insuree = db.Insurees.Find(id);
-            db.Insurees.Remove(insuree);
-            db.SaveChanges();
-            return RedirectToAction("Index");
-        }
+    
 
         protected override void Dispose(bool disposing)
         {
